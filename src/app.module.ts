474fa4f6bfd9dc9cf8c24d46152d5b77806024df
faiz140405 <cahmargablok51@gmail.com>
2026-03-03 00:35:07 +0700

    buat modul POS kategori produk dan order

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CategoryModule, ProductModule, OrderModule],
 })
 export class AppModule {}
